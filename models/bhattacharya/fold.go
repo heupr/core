2d2faaaa87ca2f11cdbd51cf1ff1d87a50ca0cca
forstmeier <john.forstmeier@gmail.com>
2016-10-08 17:56:50 -0400

    Write 10-fold cross validation functionality

---
+++ models/bhattacharya/fold.go
@@ -0,0 +1,40 @@
+package bhattacharya
+
+import (
+	"coralreef-ci/models/issues"
+	"errors"
+	"math"
+)
+
+func (model *Model) Fold(issues []issues.Issue) (float64, error) {
+	issueCount := len(issues)
+	if issueCount < 10 {
+		return 0.00, errors.New("LESS THAN 10 ISSUES SUBMITTED")
+	}
+
+	score := 0.00
+
+	for i := 0.10; i < 1.00; i += 0.10 {
+		correct := 0
+
+		trainCount := int(Round(i * float64(issueCount)))
+		testCount := issueCount - trainCount
+
+		model.Learn(issues[0:trainCount])
+
+		for j := trainCount + 1; j < testCount; j++ {
+			assignee := model.Predict(issues[j])
+			if assignee == issues[j].Assignee {
+				correct += 1
+			} else {
+				continue
+			}
+		}
+		score += float64(correct) / float64(issueCount)
+	}
+	return score / 10.00, nil
+}
+
+func Round(input float64) float64 {
+	return math.Floor(input + 0.5)
+}
---
+++ models/bhattacharya/fold_test.go
@@ -0,0 +1,43 @@
+package bhattacharya
+
+import (
+	"coralreef-ci/models/issues"
+	"testing"
+)
+
+var testingIssues = []issues.Issue{
+	{Assignee: "Mike", Body: "I hail from the state of Montana"},
+	{Assignee: "Woz", Body: "Boyd, I'm wild and wooly and rough"},
+	{Assignee: "John", Body: "I ride the broncos drink and smoke"},
+	{Assignee: "Mike", Body: "Do everything that's tough"},
+	{Assignee: "Woz", Body: "I'm always lighthearted and free from care"},
+	{Assignee: "John", Body: "to a friend I'm always true"},
+	{Assignee: "Mike", Body: "You'll always find me ready to fight"},
+	{Assignee: "Woz", Body: "For dear old Sigma Nu!"},
+	{Assignee: "Mike", Body: "Who am I sir"},
+	{Assignee: "John", Body: "A fraternity man am I"},
+	{Assignee: "Woz", Body: "A Sigma Nu sir"},
+	{Assignee: "Mike", Body: "And will be until I die"},
+	{Assignee: "John", Body: "Hi-rickety whoopty do"},
+	{Assignee: "Woz", Body: "What's the matter with Sigma Nu"},
+	{Assignee: "Mike", Body: "Hallabaloo, terrikahoo"},
+	{Assignee: "John", Body: "All together for Sigma Nu!"},
+	{Assignee: "Mike", Body: "The Sigma Nu's they like to live"},
+	{Assignee: "Woz", Body: "But when it comes to die"},
+	{Assignee: "John", Body: "You'll never hear them groan or moan"},
+	{Assignee: "Mike", Body: "You'll never hear them sigh"},
+	{Assignee: "Woz", Body: "They march right up to the pearly gate"},
+	{Assignee: "John", Body: "You bet your life they do"},
+	{Assignee: "Mike", Body: "For at the gate, the meet Saint Pete"},
+	{Assignee: "Woz", Body: "And he's a Sigma Nu!"},
+}
+
+func TestFold(t *testing.T) {
+	nbModel := Model{classifier: &NBClassifier{}}
+	result, _ := nbModel.Fold(testingIssues)
+	if result < 0.00 && result > 1.00 {
+		t.Error("\nRESULT IS OUTSIDE ACCEPTABLE RANGE",
+			"\nEXPECTED BETWEEN 0.00 AND 1.00",
+			"\nACTUAL: %f", result)
+	}
+}
