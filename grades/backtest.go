57aaeb57ae15a8a590f641d2986af81fd47ead31
forstmeier <john.forstmeier@gmail.com>
2016-10-09 13:05:49 -0400

    Minor printout changes to output for backtest

--- grades/backtest.go
+++ grades/backtest.go
@@ -1,20 +1,20 @@
 package grades
 
 import (
-  "encoding/csv"
-  "fmt"
-  "os"
 	"coralreef-ci/models/bhattacharya"
-  "coralreef-ci/models/issues"
+	"coralreef-ci/models/issues"
+	"encoding/csv"
+	"fmt"
+	"os"
 )
 
 type TestContext struct {
-  File string
-  Model bhattacharya.Model
+	File  string
+	Model bhattacharya.Model
 }
 
 type BackTestRunner struct {
-  Context TestContext
+	Context TestContext
 }
 
 func readFile(filePath string) []issues.Issue {
@@ -36,37 +36,38 @@ func readFile(filePath string) []issues.Issue {
 	return repoIssues
 }
 
-func(t *BackTestRunner) Run() {
-  filePath := t.Context.File
-  trainingSet := readFile(filePath)
-  testComparsionSet := make([]issues.Issue, len(trainingSet))
-  testSet := make([]issues.Issue, len(trainingSet))
-  copy(testSet, trainingSet)
-  copy(testComparsionSet, trainingSet)
-  t.Context.Model.Learn(trainingSet)
-  correctCount := 0
-  for i := 0; i < len(trainingSet); i++ {
-    assignees := t.Context.Model.Predict(testSet[i])
-  /*  testSet[i].Assignee = assignees[0]
-    fmt.Println("BackTest Assignee: ", testSet[i].Assignee)
-    fmt.Println("Expected Assignee: ", testComparsionSet[i].Assignee)
-    if (testComparsionSet[i].Assignee == assignees[0]) {
-      correctCount++
-      //break
-    } */
+func (t *BackTestRunner) Run() {
+	filePath := t.Context.File
+	trainingSet := readFile(filePath)
+	testComparsionSet := make([]issues.Issue, len(trainingSet))
+	testSet := make([]issues.Issue, len(trainingSet))
+	copy(testSet, trainingSet)
+	copy(testComparsionSet, trainingSet)
+	t.Context.Model.Learn(trainingSet)
+	correctCount := 0
+	for i := 0; i < len(trainingSet); i++ {
+		assignees := t.Context.Model.Predict(testSet[i])
+		/*  testSet[i].Assignee = assignees[0]
+		    fmt.Println("BackTest Assignee: ", testSet[i].Assignee)
+		    fmt.Println("Expected Assignee: ", testComparsionSet[i].Assignee)
+		    if (testComparsionSet[i].Assignee == assignees[0]) {
+		      correctCount++
+		      //break
+		    } */
 
-    for j := 0; j < len(assignees); j++ {
-      testSet[i].Assignee = assignees[j]
-      if testComparsionSet[i].Assignee == assignees[j] {
-        correctCount++
-        break;
-      }
-    }
-  }
+		for j := 0; j < len(assignees); j++ {
+			testSet[i].Assignee = assignees[j]
+			if testComparsionSet[i].Assignee == assignees[j] {
+				correctCount++
+				break
+			}
+		}
+	}
 
-  matrix,_ := bhattacharya.BuildMatrix(trainingSet, testSet)
-  bhattacharya.FullSummary(matrix)
+	matrix, _ := bhattacharya.BuildMatrix(trainingSet, testSet)
+	bhattacharya.FullSummary(matrix)
 
-  fmt.Printf("\nAccuracy: %f", float64(correctCount)/float64(len(testSet)))
-  fmt.Printf("\nCorrect Count: %d Total Count: %d", correctCount, len(testSet))
+	fmt.Printf("\nACCURACY: %f\n", float64(correctCount)/float64(len(testSet)))
+	fmt.Printf("\nCORRECT COUNT: %d\n", correctCount)
+	fmt.Printf("\nTOTAL COUNT: %d\n", len(testSet))
 }
--- grades/cmd/main.go
+++ grades/cmd/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-  "coralreef-ci/grades"
-  "coralreef-ci/models/bhattacharya"
+	"coralreef-ci/grades"
+	"coralreef-ci/models/bhattacharya"
 )
 
 func main() {
-  nbModel := bhattacharya.Model{Classifier: &bhattacharya.NBClassifier{}}
-  testContext := grades.TestContext{File: "/home/michael/golang/src/coralreef-ci/data/training/static/trainingset_corefx", Model: nbModel}
-  testRunner := grades.BackTestRunner{Context: testContext}
-  testRunner.Run()
+	nbModel := bhattacharya.Model{Classifier: &bhattacharya.NBClassifier{}}
+	testContext := grades.TestContext{File: "/home/forstmeier/Dropbox/go/src/coralreef-ci/data/training/static/trainingset_corefx", Model: nbModel}
+	testRunner := grades.BackTestRunner{Context: testContext}
+	testRunner.Run()
 }
