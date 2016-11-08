c182ee69e3ebb2dbd5b5ef0e67c1745d0b40f2b4
forstmeier <john.forstmeier@gmail.com>
2016-11-07 21:51:16 -0500

    Implement unit test for stopwords removal
    Description:
    This is a small unit test that was not included when the function was
    initially written.
    
    Warnings:
    The test is not very exhaustive as it only looks for stopwords "this", "with",
    and "the".
    
    Build well

--- models/bhattacharya/stopwords_test.go
+++ models/bhattacharya/stopwords_test.go
@@ -1,1 +1,20 @@
 package bhattacharya
+
+import (
+    "strings"
+    "testing"
+)
+
+var withStopwords = "This is a sentence with stopwords in it, the best ever."
+var withoutStopwords = "sentence stopwords"
+
+func TestRemoveStopWords(t *testing.T) {
+    output := RemoveStopWords(withStopwords)
+    if strings.Contains(output, "this") || strings.Contains(output, "with") || strings.Contains(output, "the") {
+        t.Error(
+            "\nSTOPWORDS NOT REMOVED",
+            "\nEXPECTED: ", withoutStopwords,
+            "\nACTUAL:   ", output,
+        )
+    }
+}
