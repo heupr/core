01192ebc9bc92133cfe873693850f52341105b38
taylor <mike.j.taylor0@gmail.com>
2016-12-04 19:42:51 -0500

    Backtest Performance Test Checkin
    Example run
    0) go build
    1) ./main
    2) go tool pprof -png ./main /tmp/profile080776987/cpu.pprof >> perf.pdf

--- grades/cmd/main.go
+++ grades/cmd/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"coralreefci/grades"
 	"coralreefci/models/bhattacharya"
+	"github.com/pkg/profile"
 )
 
 func main() {
+	defer profile.Start().Stop()
 	logger := bhattacharya.CreateLog("bhattacharya-backtest")
 	nbModel := bhattacharya.Model{Classifier: &bhattacharya.NBClassifier{Logger: &logger}, Logger: &logger}
 	testContext := grades.TestContext{Model: nbModel}
