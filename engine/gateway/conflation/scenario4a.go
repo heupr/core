37b3abe7bd1d9443a38c8bb132546f59e133320e
forstmeier <john.forstmeier@gmail.com>
2016-12-07 21:33:47 -0500

    Update in-file documentation
    Description:
    This clarifies the intended purpose of the scenario.
    
    Build well

--- engine/gateway/conflation/scenario4a.go
+++ engine/gateway/conflation/scenario4a.go
@@ -8,6 +8,9 @@ type Scenario4a struct {
 	Words int
 }
 
+// DOC: Scenario4a is designed to filter for issues with a specified number of
+//      words in the text body.
+//      Note that this value is set in the Scenario4a Words struct value.
 func (s *Scenario4a) Filter(expandedIssue *ExpandedIssue) bool {
 	if strings.Count(*expandedIssue.Issue.Body, " ")+1 >= s.Words {
 		return true
