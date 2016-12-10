92ed7c7894e3a7e96314cea0b4b6b610841d2454
forstmeier <john.forstmeier@gmail.com>
2016-12-10 16:00:23 -0500

    Restructure logging output
    Description:
    This implements 1) the new logging output subdirectories and 2) a unit test for
    logging. There is included logic for handling logic for flags indicating
    backtests for CoralReefLogger struct.
    
    Note: the unit test is passing.
    Note: output for caches/ has not been tests (although in theory the new output
          directory string should generate the files in the desired location).
    
    Build well

--- models/bhattacharya/log.go
+++ models/bhattacharya/log.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
 type CoralReefLogger struct {
@@ -14,6 +16,8 @@ type CoralReefLogger struct {
 	Backtest bool
 }
 
+// DOC: CreateLog generates a new proprietary logger structure.
+//      Note that the "prefix" set in the logger is just the log file name.
 func CreateLog(name string, backtest bool) CoralReefLogger {
 	buf := bytes.Buffer{}
 	logger := log.New(&buf, name, log.Lshortfile)
@@ -21,20 +25,44 @@ func CreateLog(name string, backtest bool) CoralReefLogger {
 	return newLog
 }
 
+// DOC: Log calls the logging input functionality.
 func (crl *CoralReefLogger) Log(input interface{}) {
 	crl.CRLog.Print(input)
 }
 
+// DOC: Flush closes out and writes the results of the log to the desired file.
 func (crl *CoralReefLogger) Flush() {
-	output := ""
-	if crl.Backtest {
-		output = "../../data/backtests/" + crl.Name
-	} else {
-		output = "../../data/logs/" + crl.Name
-	}
-	f, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
-	if err != nil {
-		panic(err)
-	}
-	io.WriteString(f, crl.CRBuff.String())
+	n := time.Now()
+	y, m, d := n.Date()
+	filename := crl.Name + "-" + strconv.Itoa(y) + "-" + m.String() + "-" + strconv.Itoa(d)
+
+    // DOC: This logic evalutes whether the log should be generated in the
+    //      backtests/ or logs/ directories.
+    if crl.Backtest {
+        crl.pathfinder("../../data/backtests/" + filename, n)
+    } else {
+        crl.pathfinder("../../data/logs/" + filename, n)
+    }
+
+}
+
+// DOC: pathfinder is a helper method designed to perform the necessary file
+//      creation / writing processes. Note that the logic here simply
+//      evaluates whether the file exists in the target path.
+func (crl *CoralReefLogger) pathfinder(filepath string, t time.Time) {
+    h, m, _ := t.Clock()
+    header := "LOG OUTPUT TIME\nHOUR: " + strconv.Itoa(h) + " " + "MINUTE: " + strconv.Itoa(m)
+    if _, err := os.Stat(filepath); err == nil {
+        f, e := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY, 0600)
+        if e != nil {
+            panic(e)
+        }
+        io.WriteString(f, header + "\n\n" + crl.CRBuff.String() + "\n\n")
+    } else {
+        f, e := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+        if e != nil {
+            panic(e)
+        }
+        io.WriteString(f, header + "\n\n" + crl.CRBuff.String() + "\n\n")
+    }
 }
