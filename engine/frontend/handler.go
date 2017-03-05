ff95aae562a89abf452a447e6675940e71ec4367
forstmeier <john.forstmeier@gmail.com>
2017-03-05 15:02:52 -0500

    Repair known unit test error
    Description:
    Updated code to reflect recent broader changes to the frontend assets is
    implemented in the unit tests. Also a known runtime panic is corrected.
    
    Note: All code within frontend/ is passing unit tests.
    Note: The unit tests are not necessarily exhaustive / all complete.
    Note: Formatting has been applied.
    
    Build well

--- engine/frontend/handler.go
+++ engine/frontend/handler.go
@@ -7,41 +7,30 @@ import (
 )
 
 const secretKey = "chalmun"
+
 var Workload = make(chan github.IssuesEvent, 100)
 
 func collectorHandler() http.Handler {
-	// NOTE: Temporarily removed the "secret" argument - eventually implement
-	//       for security purposes.
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		eventType := r.Header.Get("X-Github-Event")
 		if eventType != "issues" {
 			fmt.Printf("Ignoring '%v' event", eventType)
 			return
 		}
-        payload, err := github.ValidatePayload(r, []byte(secretKey))
+		payload, err := github.ValidatePayload(r, []byte(secretKey))
 		if err != nil {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
-        event, err := github.ParseWebHook(github.WebHookType(r), payload)
+		event, err := github.ParseWebHook(github.WebHookType(r), payload)
 
-        if err != nil {
-    		fmt.Printf("Could not parse webhook %v", err)
-    		return
-    	}
+		if err != nil {
+			fmt.Printf("Could not parse webhook %v", err)
+			return
+		}
 
-		// event := github.IssueEvent{}
-		// err = json.Unmarshal(body, &event)
-        // err = json.Unmarshal(payload, &event)
-        // if err != nil {
-		// 	fmt.Printf("Ignoring '%s' event with invalid payload", eventType)
-		// 	http.Error(w, "Bad request", http.StatusBadRequest)
-		// 	return
-		// }
-		// fmt.Printf("Handling '%s' event for %s", eventType, repo)
-
-        issueEvent := event.(github.IssuesEvent)
-        Workload <- issueEvent
+		// issueEvent := *event.(*github.IssuesEvent)
+		Workload <- *event.(*github.IssuesEvent)
 	})
 }
--- engine/frontend/handler_test.go
+++ engine/frontend/handler_test.go
@@ -1,20 +1,20 @@
 package frontend
 
 import (
-    "bytes"
-    "crypto/hmac"
-    "crypto/sha1"
-    "encoding/hex"
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func Test_collectorHandler(t *testing.T) {
-    testBody := `{"id":1}`
-    rec := httptest.NewRecorder()
-    buf := bytes.NewBufferString(testBody)
-    req, err := http.NewRequest("POST", "/handler-test", buf)
+	testBody := `{"id":1}`
+	rec := httptest.NewRecorder()
+	buf := bytes.NewBufferString(testBody)
+	req, err := http.NewRequest("POST", "/handler-test", buf)
 	if err != nil {
 		t.Error(
 			"Failure generating testing request",
@@ -22,46 +22,15 @@ func Test_collectorHandler(t *testing.T) {
 		)
 	}
 
-    req.Header.Set("X-Github-Event", "issues")
-
-    // sig := "sha1=" + hex.EncodeToString([]byte("825526bac599fa432336d57653a562142ffa94af"))
-
-    // sig := "sha1=825526bac599fa432336d57653a562142ffa94af"
-
-    mac := hmac.New(sha1.New, []byte(secretKey))
+	req.Header.Set("X-Github-Event", "issues")
+	mac := hmac.New(sha1.New, []byte(secretKey))
 	mac.Write([]byte(testBody))
-	// s := mac.Sum(nil)
+	sig := "sha1=" + hex.EncodeToString(mac.Sum(nil))
+	req.Header.Set("X-Hub-Signature", sig)
 
-    // h := sha1.New()
-    // io.WriteString(h, secretKey)
-    // io.WriteString(h)
-    sig := "sha1=" + hex.EncodeToString(mac.Sum(nil))
-
-    // TEMPORARY
-    // mac := hmac.New(sha1.New, key)
-    // mac.Write(message)
-    // mac.Sum(nil)
-    // sig := "sha1=" + hex.EncodeToString([]byte(secretKey))
-
-    req.Header.Set("X-Hub-Signature", sig)
-    // fmt.Println(req)                                 // TEMPORARY
-    // fmt.Println(req.Header.Get("X-Hub-Signature"))   // TEMPORARY
-    handler := collectorHandler("")
+	handler := collectorHandler()
 	handler.ServeHTTP(rec, req)
 	if status := rec.Code; status != http.StatusOK {
 		t.Errorf("Handler returning incorrect status code; returning %v", status)
 	}
 }
-
-/*
-// e.g. GET /api/projects?page=1&per_page=100
-  req, err := http.NewRequest("GET", "/api/projects",
-      // Note: url.Values is a map[string][]string
-      url.Values{"page": {"1"}, "per_page": {"100"}})
-  if err != nil {
-      t.Fatal(err)
-  }
-
-  // Our handler might also expect an API key.
-  req.Header.Set("Authorization", "Bearer abc123")
-*/
