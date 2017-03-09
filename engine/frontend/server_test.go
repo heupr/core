499cc529fc5e1cc12a370d4272a2778747325c27
forstmeier <john.forstmeier@gmail.com>
2017-03-08 22:16:07 -0500

    Write in-code server documentation
    Description:
    Cleaning up TODOs and other notes regarding code snippets to address.
    
    Build well

--- engine/frontend/server_test.go
+++ engine/frontend/server_test.go
@@ -3,5 +3,7 @@ package frontend
 import "testing"
 
 func TestStart(t *testing.T) {
-
+	// TODO: For simplification purposes, we may want to place some "global"
+	//       testing variables in this file so that we do not need to
+	//       constantly redeclare them throughout the unit tests.
 }
