152df83a87617d845d8031abb4dc74c1064f56b2
forstmeier <john.forstmeier@gmail.com>
2017-03-27 22:40:35 -0400

    Build GitHub Archive replay assets
    Description:
    This code reads from a target folder holding the gzip files downloaded from the
    GitHub Archive project website. Ideally these files will always be gzip (even
    after being grepped for only "IssuesEvent" payloads).
    
    Note: The unit test passes but is not executing quickly
    Note: Current unit tests are not comprehensive (see in-code notes)
    
    Build well

--- engine/frontend/testing/backtest/reader_test.go
+++ engine/frontend/testing/backtest/reader_test.go
@@ -2,10 +2,14 @@ package backtest
 
 import "testing"
 
-func TestDirectoryWalk(t *testing.T) {
-    tr := ReplayServer{}
-    err := tr.DirectoryWalk()
-    if err != nil {
-        t.Errorf("ERROR: %v", err)
-    }
+func TestWalkArchive(t *testing.T) {
+	tr := ReplayServer{}
+	err := tr.WalkArchive("/home/forstmeier/Downloads/test")
+	if err != nil {
+		t.Errorf("ERROR: %v", err)
+	}
 }
+
+// TODO: various unit test scenarios
+// - feed in non-gz file
+// - only non-IssuesEvent objects in file
