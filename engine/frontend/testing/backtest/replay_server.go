e003fb4206df826193902782d59aa1bbc9ee70a3
forstmeier <john.forstmeier@gmail.com>
2017-04-08 17:07:10 -0400

    Update unit test for new source code
    Description:
    parseFile unit tests have been implemented here; several check for failing
    while a final one ensures it is operating correctly (by passing in a full
    gzipped file w/ contents).
    
    Note: Legacy test code is included as commented assets.
    Note: All unit tests are passing within these committed files.
    
    Build well

--- engine/frontend/testing/backtest/replay_server.go
+++ engine/frontend/testing/backtest/replay_server.go
@@ -2,128 +2,160 @@ package backtest
 
 import (
 	"bytes"
-    "compress/gzip"
+	"compress/gzip"
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/hex"
-    "encoding/json"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
-    "net/url"
-    "os"
-    "path/filepath"
-	"time"
+	"net/url"
+	"os"
+	"path/filepath"
+	// "time"
 
-    "github.com/google/go-github/github"
+	"github.com/google/go-github/github"
 
-    "coralreefci/models"
+	"coralreefci/engine/frontend"
+	// "coralreefci/models"
 )
 
+// [X] COMPLETED
 const (
-    secretKey = "chalmun"
-    localPath = "http://localhost:8080/"
-    modelList = []models.Model{}
+	secretKey = "chalmun"
+	localPath = "http://localhost:8080/"
 )
 
+// [X] COMPLETED
+var modelList = []*frontend.HeuprModel{}
+
+// [X] COMPLETED
 type Event struct {
 	Type    string          `json:"type"`
 	Payload json.RawMessage `json:"payload"`
 }
 
+// [X] COMPLETED
 type BacktestServer struct {
-    HeuprServer
-    Archive []Event
+	frontend.HeuprServer
+	Archive []Event
 }
 
+// [ ] COMPLETED
+// - [ ] unit test
 func (b *BacktestServer) loadArchive() {
-    args := os.Args
-    for _, arg := range args[1:] {
-        fn, _ := os.Stat(arg)
-        switch mode := fn.Mode(); {
-        case mode.IsDir():
-            err := filepath.Walk(arg, func(path string, f os.FileInfo, err error) error {
-                b.parseFile(path)
-                return nil
-            })
-        case mode.IsRegular():
-            b.parseFile(arg)
-        default:
-            fmt.Println("Unrecognized argument; provide a file or directory")
-        }
-    }
+	args := os.Args
+	for _, arg := range args[1:] {
+		fn, _ := os.Stat(arg)
+		switch mode := fn.Mode(); {
+		case mode.IsDir():
+			err := filepath.Walk(arg, func(path string, f os.FileInfo, err error) error {
+				b.parseFile(path)
+				return nil
+			})
+			if err != nil {
+				fmt.Printf("Error walking directory: %v", err)
+			}
+		case mode.IsRegular():
+			b.parseFile(arg)
+		default:
+			fmt.Println("Unrecognized argument; provide a file or directory")
+		}
+	}
 }
 
-func (b *BacktestServer) parseFile(filename string) {
-    f, _ := os.Open(filename)
-    if err != nil {
-        return err
-    }
-    defer f.Close()
-    gr, err := gzip.NewReader(f)
-    if err != nil {
-        return err
-    }
-    defer gr.Close()
-    jd := json.NewDecoder(gr)
-    for {
-        e := Event{}
-        if err := jd.Decode(&e); err == io.EOF {
-            break
-        } else if err != nil {
-            return err
-        }
-        b.Archive = append(b.Archive, e)
-    }
+// [ ] COMPLETED
+// - [ ] unit test
+func (b *BacktestServer) parseFile(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	gr, err := gzip.NewReader(f)
+	if err != nil {
+		return err
+	}
+	defer gr.Close()
+	jd := json.NewDecoder(gr)
+	for {
+		e := Event{}
+		if err := jd.Decode(&e); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		b.Archive = append(b.Archive, e)
+	}
+	return nil
 }
 
+// [ ] COMPLETED
+// - [ ] unit test
 func (b *BacktestServer) loadRepos() {
-    // TODO: parsing some input
-    // defines the desired repos to run
-    // specific GitHub Repositories
-    repos = []github.Repositories{} // TEMPORARY
+	// TODO: parsing some input (likely a file w/ JSON content)
+	// - defines the desired repos to run (specific GitHub Repositories)
+	repos := []github.Repository{} // TEMPORARY
 
-    client := github.NewClient(nil)
-    u, _ := url.Parse(localPath)
-    client.BaseURL = u
-    client.UploadURL = u
+	client := github.NewClient(nil)
+	u, _ := url.Parse(localPath)
+	client.BaseURL = u
+	client.UploadURL = u
 
-    for i := 0; i < len(repos); i ++ {
-        b.Repos[i] = &HeuprRepo{
-            Repo: &repos[i]
-            Hive: &HeuprHive{Models: &models}
-            Client: &client,
-        }
-    }
+	for i := 0; i < len(repos); i++ {
+		b.Repos[i] = &frontend.HeuprRepo{
+			Repo:   &repos[i],
+			Hive:   &frontend.HeuprHive{Models: modelList},
+			Client: client,
+		}
+	}
 }
 
-func (b *BacktestServer) backtestHandler(w *http.ResponseWriter, r *http.Request) {
-    // TODO: Review to see if there is anything else that needs to be done to
-    //       the individual objects prior to being written to the
-    //       ResponseWriter.
-    for _, v := range h.Archive {
-        fmt.Fprint(w, v)
-    }
+// [ ] COMPLETED
+// - [ ] unit test
+func (b *BacktestServer) backtestHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: Review to see if there is anything else that needs to be done to
+	//       the individual objects prior to being written to the
+	//       ResponseWriter.
+	half := len(b.Archive) / 2
+	for _, v := range b.Archive[:half] {
+		fmt.Fprint(w, v)
+	}
 }
 
+// [ ] COMPLETED
+// - [ ] complete source code
+// - [ ] unit test
+func (b *BacktestServer) backtestPredict(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// [ ] COMPLETED
+// - [ ] parse payload for repo ID
+// - [ ] unit test
 // TODO: ngrok url is now located here and in hooker.go (lets fix that with an
 //       env variable. Fortunately ngrok is written in Golang (so that helps))
 // TODO: Per Gor Replay File Add Missing HTTP Headers (File in Slack Channel - requests_0.gor)
 // TODO: (see unit test file for more TODOS)
 // TODO: Perf: Reuse Http Request objects
-func (r *ReplayServer) HTTPPost(payload *bytes.Buffer) {
-    req, err := http.NewRequest("POST", "http://5b0f0030.ngrok.io/hook", payload)
-    if err != nil {
-        fmt.Println(err)
-    }
-    req.Header.Set("X-Github-Event", "issues")
-    req.Header.Set("X-GitHub-Delivery", "placeholder")
-    req.Header.Set("content-type", "application/json")
-    mac := hmac.New(sha1.New, []byte(secretKey))
-    mac.Write(payload.Bytes())
-    sig := "sha1=" + hex.EncodeToString(mac.Sum(nil))
-    req.Header.Set("X-Hub-Signature", sig)
+func (b *BacktestServer) HTTPPost(payload *bytes.Buffer) {
+	req, err := http.NewRequest("POST", localPath, payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// TODO: Parse payload to get given repo ID int
+	// plStr := payload.String()
+	req.Header.Set("X-Github-Event", "issues")
+	req.Header.Set("X-GitHub-Delivery", "placeholder")
+	req.Header.Set("content-type", "application/json")
+	mac := hmac.New(sha1.New, []byte(secretKey))
+	mac.Write(payload.Bytes())
+	sig := "sha1=" + hex.EncodeToString(mac.Sum(nil))
+	req.Header.Set("X-Hub-Signature", sig)
 
-    // TODO: Note that this will need to change if the default address needs
-    // to be overwritten (e.g. r.client.NewRequest())
-    r.client.Do(req)
+	id := 1
+	// TODO: Pass repo ID into id
+	// TODO: Check to make sure Do() is having the right values passed in
+	b.Repos[id].Client.Do(req, payload)
 }
