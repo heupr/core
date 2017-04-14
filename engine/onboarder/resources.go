ad68b0e252e8f28ecd9daa92ff2b3570d84954dc
forstmeier <john.forstmeier@gmail.com>
2017-04-14 17:32:01 -0400

    Implement pagination check for resource pull
    Description:
    I am not 100% sure that this was necessary but I completed it as
    part of an existing in-code TODO; if it needs to be removed later,
    so be it.
    
    Note: All unit tests are passing in onboarder/
    
    Build well

--- engine/onboarder/resources.go
+++ engine/onboarder/resources.go
@@ -5,12 +5,22 @@ import "github.com/google/go-github/github"
 func listRepositories(client *github.Client) ([]*github.Repository, error) {
 	opts := &github.RepositoryListOptions{
 		Type: "all",
+		ListOptions: github.ListOptions{
+			PerPage: 100,
+		},
 	}
-	// TODO: Evaluate ^ to ensure that this is an exhaustive list (e.g. does
-	//       not need pagination).
-	repos, _, err := client.Repositories.List("", opts)
-	if err != nil {
-		return nil, err
+	allRepos := []*github.Repository{}
+	for {
+		repos, resp, err := client.Repositories.List("", opts)
+		if err != nil {
+			return nil, err
+		}
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		} else {
+			opts.ListOptions.Page = resp.NextPage
+		}
 	}
-	return repos, nil
+	return allRepos, nil
 }
