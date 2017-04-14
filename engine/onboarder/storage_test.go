b5a4227ab7f8aa58f08a5ac3f48640674012905c
forstmeier <john.forstmeier@gmail.com>
2017-04-14 17:35:48 -0400

    Minor code cleanup in test file
    Build well

--- engine/onboarder/storage_test.go
+++ engine/onboarder/storage_test.go
@@ -15,7 +15,7 @@ func Test_open(t *testing.T) {
 	}
 
 	if err != nil {
-		t.Errorf("Error opening new database instance; %v", err)
+		t.Errorf("Error opening new database instance: %v", err)
 	}
 	t.Run("store", func(t *testing.T) {
 		err := testServer.Database.store(testBucket, testKey, testValue)
@@ -26,25 +26,19 @@ func Test_open(t *testing.T) {
 	t.Run("retrieve", func(t *testing.T) {
 		value, err := testServer.Database.retrieve(testBucket, testKey)
 		if err != nil {
-			t.Errorf(
-				"Error retrieving data from database",
-				"\nExpected %v; received %v", testValue, value,
-			)
+			t.Errorf("Error retrieving data from database - expected %v; received %v", testValue, value)
 		}
 	})
 	t.Run("bulk", func(t *testing.T) {
 		_, err := testServer.Database.retrieveBulk()
 		if err != nil {
-			t.Errorf("Error pulling all data from default database %v", err)
+			t.Errorf("Error pulling all data from default database: %v", err)
 		}
 	})
 	t.Run("delete", func(t *testing.T) {
 		err := testServer.Database.delete(testBucket)
 		if err != nil {
-			t.Errorf(
-				"Error deleting database entry",
-				"\n", err,
-			)
+			t.Errorf("Error deleting database entry: %v", err,)
 		}
 	})
 }
