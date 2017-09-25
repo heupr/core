8ec4ac61e4638f1bc0cfb2eb91cba260571464e6
taylor <mike.j.taylor0@gmail.com>
2017-09-24 22:57:51 -0400

    Hooker Fix Forgot to Include Port Number
    Description: Hooker Fix Forgot to Include Port Number. Port 8020.

--- pipeline/frontend/hooker.go
+++ pipeline/frontend/hooker.go
@@ -17,7 +17,7 @@ func (fs *FrontendServer) NewHook(repo *github.Repository, client *github.Client
 	}
 	name := *repo.Name
 	owner := *repo.Owner.Login
-	url := "http://35.196.33.44/hook"
+	url := "http://35.196.33.44:8020/hook"
 	hook, _, err := client.Repositories.CreateHook(context.Background(), owner, name, &github.Hook{
 		Name:   github.String("web"),
 		Events: []string{"issues", "pull_request"},
