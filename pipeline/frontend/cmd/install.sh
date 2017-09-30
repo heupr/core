ba31bf64386b4022651a7ebf31b576c2442ddcdb
taylor <mike.j.taylor0@gmail.com>
2017-09-30 20:40:15 +0000

    Replacing Dev SSL Key With Prod SSL Key

--- pipeline/frontend/cmd/install.sh
+++ pipeline/frontend/cmd/install.sh
@@ -5,3 +5,5 @@ cp ./prep.sh /home/heupr/golang/bin/core/pipeline/frontend/cmd/prep.sh
 cp ./configprod.yaml /home/heupr/golang/bin/core/pipeline/frontend/cmd/config.yaml
 cp ./whitelist.toml /home/heupr/golang/bin/core/pipeline/frontend/cmd/whitelist.toml
 cp -r ../website/ /home/heupr/golang/bin/core/pipeline/frontend
+cp ./heupr.key /home/heupr/golang/bin/core/pipeline/frontend/cmd/heupr.key
+cp ./heupr_io.crt /home/heupr/golang/bin/core/pipeline/frontend/cmd/heupr_io.crt
