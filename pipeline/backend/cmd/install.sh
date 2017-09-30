4315f64bbbfe16e0e842777cb6c054f084cd5416
taylor <mike.j.taylor0@gmail.com>
2017-09-30 19:29:15 +0000

    Backend Install Script Typo

--- pipeline/backend/cmd/install.sh
+++ pipeline/backend/cmd/install.sh
@@ -1,4 +1,4 @@
 cp ./cmd /home/heupr/golang/bin/core/pipeline/backend/cmd/cmd
 cp ./startprod.sh /home/heupr/golang/bin/core/pipeline/backend/cmd/start.sh
 chmod +x /home/heupr/golang/bin/core/pipeline/backend/cmd/start.sh
-cp ./configprod.yaml /home/heupr/golang/bin/core/pipeline/backend/config.yaml
+cp ./configprod.yaml /home/heupr/golang/bin/core/pipeline/backend/cmd/config.yaml
