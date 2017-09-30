3195966fe918170e3bc3a87695a6b6814a880238
taylor <mike.j.taylor0@gmail.com>
2017-09-30 20:49:14 +0000

    Frontend Prod Binary Prep Script

--- pipeline/frontend/cmd/install.sh
+++ pipeline/frontend/cmd/install.sh
@@ -1,7 +1,7 @@
 cp ./cmd /home/heupr/golang/bin/core/pipeline/frontend/cmd/cmd
 cp ./startprod.sh /home/heupr/golang/bin/core/pipeline/frontend/cmd/start.sh
 chmod +x /home/heupr/golang/bin/core/pipeline/frontend/cmd/start.sh
-cp ./prep.sh /home/heupr/golang/bin/core/pipeline/frontend/cmd/prep.sh
+cp ./prodprep.sh /home/heupr/golang/bin/core/pipeline/frontend/cmd/prep.sh
 cp ./configprod.yaml /home/heupr/golang/bin/core/pipeline/frontend/cmd/config.yaml
 cp ./whitelist.toml /home/heupr/golang/bin/core/pipeline/frontend/cmd/whitelist.toml
 cp -r ../website/ /home/heupr/golang/bin/core/pipeline/frontend
---
+++ pipeline/frontend/cmd/prodprep.sh
@@ -0,0 +1,1 @@
+sudo setcap CAP_NET_BIND_SERVICE=+eip /home/heupr/golang/bin/core/pipeline/frontend/cmd/cmd
