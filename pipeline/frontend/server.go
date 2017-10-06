146ca7a92edba0c574554fbdd3e67a07114bb8b3
forstmeier <john.forstmeier@gmail.com>
2017-10-05 22:59:34 -0400

    Add frontend server graceful shutdown
    Description:
    This may be overkill for the frontend following the switch to GitHub
    Integrations. Additionally, a field has been added to the FrontendServer struct
    to allow for the Shutdown method to be called correctly.
    
    This commit also contains a refactoring of the prod/dev loop when starting up
    servers; lastly, the outlines of a unit test for LaunchServer is included (it
    may be better to just unit test Start/Stop given the need to build .pem files).
    
    Note: Code is formatted appropriately
    Note: Code appears to be working correctly
    Note: No unit tests have been implemented for the server.go file

--- pipeline/frontend/server.go
+++ pipeline/frontend/server.go
@@ -1,11 +1,14 @@
 package frontend
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
 	"time"
 
-	"github.com/boltdb/bolt"
+	// "github.com/boltdb/bolt"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2"
 
@@ -18,7 +21,8 @@ type State struct {
 }
 
 type FrontendServer struct {
-	Server     http.Server
+	Primary    http.Server
+	Redirect   http.Server
 	httpClient http.Client
 	Database   BoltDB
 	state      State
@@ -32,80 +36,74 @@ func (fs *FrontendServer) routes() *http.ServeMux {
 	return mux
 }
 
-func (fs *FrontendServer) Start() {
-	fs.OpenBolt()
-	if err := fs.Database.Initialize(); err != nil {
-		utils.AppLog.Error("frontend server: ", zap.Error(err))
+func (fs *FrontendServer) LaunchServer(secure, unsecure, cert, key string) {
+	// Primary server with HTTPS.
+	fs.Primary = http.Server{
+		Addr:    secure,
+		Handler: fs.routes(),
+	}
+	go func() {
+		if err := fs.Primary.ListenAndServeTLS(cert, key); err != nil {
+			utils.AppLog.Error("primary server failed to start", zap.Error(err))
+			panic(err)
+		}
+	}()
+
+	// For redirection purposes only.
+	fs.Redirect = http.Server{
+		Addr:    unsecure,
+		Handler: http.HandlerFunc(httpRedirect),
+	}
+	if err := fs.Redirect.ListenAndServe(); err != nil {
+		utils.AppLog.Error("redirect server failed to start", zap.Error(err))
 		panic(err)
 	}
-	fs.CloseBolt()
+}
 
-	fs.state = State{Tokens: make(map[int]*oauth2.Token)}
-	fs.httpClient = http.Client{
-		Timeout: time.Second * 10,
-	}
+func (fs *FrontendServer) Start() {
+	// fs.OpenBolt()
+	// if err := fs.Database.Initialize(); err != nil {
+	// 	utils.AppLog.Error("frontend server: ", zap.Error(err))
+	// 	panic(err)
+	// }
+	// fs.CloseBolt()
+	//
+	// fs.state = State{Tokens: make(map[int]*oauth2.Token)}
+	// fs.httpClient = http.Client{
+	// 	Timeout: time.Second * 10,
+	// }
+
+	stopper := make(chan os.Signal)
+	signal.Notify(stopper, os.Interrupt)
 
 	if PROD {
-		// Production server with HTTPS.
-		fs.Server = http.Server{
-			Addr:    "10.142.1.0:443",
-			Handler: fs.routes(),
-		}
-		go func() {
-			if err := fs.Server.ListenAndServeTLS("heupr_io.crt", "heupr.key"); err != nil {
-				utils.AppLog.Error("frontend server failed to start", zap.Error(err))
-				panic(err)
-			}
-		}()
-
-		// For redirection purposese only.
-		unprotected := http.Server{
-			Addr:    "10.142.1.0:80",
-			Handler: http.HandlerFunc(httpRedirect),
-		}
-		if err := unprotected.ListenAndServe(); err != nil {
-			utils.AppLog.Error("unprotected server failed to start", zap.Error(err))
-			panic(err)
-		}
+		fs.LaunchServer("10.142.1.0:443", "10.142.1.0:80", "heupr_io.crt", "heupr.key")
 	} else {
-		// Dev server with HTTPS.
-		fs.Server = http.Server{
-			Addr:    "127.0.0.1:8081",
-			Handler: fs.routes(),
-		}
-		go func() {
-			if err := fs.Server.ListenAndServeTLS("cert.pem", "key.pem"); err != nil {
-				utils.AppLog.Error("frontend server failed to start", zap.Error(err))
-				panic(err)
-			}
-		}()
-
-		// For redirection purposese only.
-		unprotected := http.Server{
-			Addr:    "127.0.0.1:8080",
-			Handler: http.HandlerFunc(httpRedirect),
-		}
-		if err := unprotected.ListenAndServe(); err != nil {
-			utils.AppLog.Error("unprotected server failed to start", zap.Error(err))
-			panic(err)
-		}
+		fs.LaunchServer("127.0.0.1:8081", "127.0.0.1:8080", "cert.pem", "key.pem")
 	}
+
+	<-stopper
+	utils.AppLog.Info("keyboard interrupt received")
+	fs.Stop()
 }
 
 func (fs *FrontendServer) Stop() {
-	// TODO: Implement graceful shutdown.
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	fs.Primary.Shutdown(ctx)
+	fs.Redirect.Shutdown(ctx)
+	utils.AppLog.Info("graceful frontend shutdown")
 }
 
-func (fs *FrontendServer) OpenBolt() error {
-	boltDB, err := bolt.Open(utils.Config.BoltDBPath, 0644, nil)
-	if err != nil {
-		utils.AppLog.Error("failed opening bolt", zap.Error(err))
-		return err
-	}
-	fs.Database = BoltDB{DB: boltDB}
-	return nil
-}
-
-func (fs *FrontendServer) CloseBolt() {
-	fs.Database.DB.Close()
-}
+// func (fs *FrontendServer) OpenBolt() error {
+// 	boltDB, err := bolt.Open(utils.Config.BoltDBPath, 0644, nil)
+// 	if err != nil {
+// 		utils.AppLog.Error("failed opening bolt", zap.Error(err))
+// 		return err
+// 	}
+// 	fs.Database = BoltDB{DB: boltDB}
+// 	return nil
+// }
+//
+// func (fs *FrontendServer) CloseBolt() {
+// 	fs.Database.DB.Close()
+// }
