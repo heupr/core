2dbf03d4977961355604f97759aed825df826d42
forstmeier <john.forstmeier@gmail.com>
2017-11-06 21:53:33 -0500

    Repair outdated unit tests
    Description:
    Again, as part of the transition to an Integration, these unit tests needed to
    be repaired. Now there are unit tests individually reflecting each of the
    handlers that check for statuses.
    
    Note: Updating unit tests
    Note: The httpRedirect unit test is only checking the non-PROD result (but this
          should be acceptable)
    Note: Code has been formatted appropriately
    Note: Code has been linted correctly

--- pipeline/frontend/callback_test.go
+++ pipeline/frontend/callback_test.go
@@ -4,42 +4,54 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
-	"golang.org/x/oauth2"
 )
 
-func Test_githubCallbackHandler(t *testing.T) {
-	testFS := new(FrontendServer)
-	testFS.state.Tokens = make(map[int]*oauth2.Token)
-
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
-		w.Write([]byte("access_token=039f5f2f98a87f46abef10170866ed8ecf3b5b2d&scope=user&token_type=bearer"))
-	}))
-
-	oaConfig = &oauth2.Config{
-		ClientID:     "TEST_ID",
-		ClientSecret: "TEST_SECRET",
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  ts.URL + "/auth",
-			TokenURL: ts.URL + "/token",
-		},
-		Scopes: []string{"test-scope"},
-	}
-
-	testURL := oaConfig.AuthCodeURL(oaState, oauth2.AccessTypeOffline)
-
+func Test_httpRedirect(t *testing.T) {
 	rec := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", testURL, nil)
+	req, err := http.NewRequest("GET", "/test-url", nil)
 	if err != nil {
 		t.Errorf("failure generating test request: %v", err)
 	}
 
-	handler := http.HandlerFunc(testFS.githubCallbackHandler)
+	handler := http.HandlerFunc(httpRedirect)
+	handler.ServeHTTP(rec, req)
+
+	// This redirects from the non-PROD else check in the production code.
+	wantedStatus := http.StatusMovedPermanently
+	if receivedStatus := rec.Code; receivedStatus != wantedStatus {
+		t.Errorf(
+			"handler returning incorrect status code; received %v, wanted %v",
+			receivedStatus, wantedStatus,
+		)
+	}
+
+}
+
+func Test_setupCompleteHandler(t *testing.T) {
+	testFS := new(FrontendServer)
+
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "/test-url", nil)
+	if err != nil {
+		t.Errorf("failure generating test request: %v", err)
+	}
+
+	handler := http.HandlerFunc(testFS.setupCompleteHandler)
 	handler.ServeHTTP(rec, req)
 
 	wantedStatus := http.StatusOK
 	if receivedStatus := rec.Code; receivedStatus != wantedStatus {
-		t.Errorf("handler returning incorrect status code; received %v, wanted %v", receivedStatus, wantedStatus)
+		t.Errorf(
+			"handler returning incorrect status code; received %v, wanted %v",
+			receivedStatus, wantedStatus,
+		)
+	}
+
+	// Check that the response body is correct.
+	if rec.Body.String() != setup {
+		t.Errorf(
+			"incorrect response body\n%v",
+			rec.Body.String(),
+		)
 	}
 }
