c8811259a17313788eaa5542983a0f369adb1dae
forstmeier <john.forstmeier@gmail.com>
2017-12-17 11:58:18 -0500

    Remove legacy setup complete design
    Description:
    The setup complete handler needed additional formatting now it references a
    file in the website2 (temporary) folder instead of a variable in the Go code.
    
    Note: WIP check-in
    Note: Relevant unit tests have been updated (to setupCompleteHandler)
    Note: Not all unit tests are passing
    Note: Progress on #51

--- pipeline/frontend/handlers.go
+++ pipeline/frontend/handlers.go
@@ -10,18 +10,18 @@ import (
 	"core/utils"
 )
 
-const setup = `
-<html>
-    <title>
-        Heupr
-    </title>
-    <body>
-        <p>Awesome! Setup is complete!</p>
-        <p>Issue assignments will go out in a few minutes through GitHub</p>
-        <p>Return to the <a href="/">main page</a></p>
-    </body>
-</html>
-`
+func httpRedirect(w http.ResponseWriter, r *http.Request) {
+	if PROD {
+		http.Redirect(w, r, "https://heupr.io", http.StatusMovedPermanently)
+	} else {
+		http.Redirect(
+			w,
+			r,
+			"https://127.0.0.1:8081",
+			http.StatusMovedPermanently,
+		)
+	}
+}
 
 func staticHandler(filepath string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -42,32 +42,32 @@ func staticHandler(filepath string) http.HandlerFunc {
 	})
 }
 
-var decoder = schema.NewDecoder()
-
-var mainHandler = http.StripPrefix(
-	"/",
-	http.FileServer(http.Dir("../website/")),
-)
-
-func httpRedirect(w http.ResponseWriter, r *http.Request) {
-	if PROD {
-		http.Redirect(w, r, "https://heupr.io", http.StatusMovedPermanently)
-	} else {
-		http.Redirect(
-			w,
-			r,
-			"https://127.0.0.1:8081",
-			http.StatusMovedPermanently,
-		)
-	}
-}
-
 func setupCompleteHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("website2/setup-complete.html")
+	if err != nil {
+		if PROD {
+			utils.SlackLog.Error(
+				"Error generating setup complete page",
+				zap.Error(err),
+			)
+		}
+		http.Redirect(w, r, "/", http.StatusInternalServerError)
+		return
+	}
 	utils.AppLog.Info("Completed user signed up")
 	if PROD {
 		utils.SlackLog.Info("Completed user signed up")
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(setup))
+	w.Write([]byte(string(data)))
 }
+
+// NOTE: Depreciate this code.
+var decoder = schema.NewDecoder()
+
+// NOTE: Depreciate this code.
+var mainHandler = http.StripPrefix(
+	"/",
+	http.FileServer(http.Dir("../website/")),
+)
--- pipeline/frontend/handlers_test.go
+++ pipeline/frontend/handlers_test.go
@@ -2,6 +2,7 @@ package frontend
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -17,30 +18,6 @@ func init() {
 		fmt.Printf("failure generating test request: %v", err)
 	}
 	*req = *r
-}
-
-func Test_staticHandler(t *testing.T) {
-	assert := assert.New(t)
-    
-    // More files/scenarios can be added here as desired.
-    tests := []struct {
-		filepath string
-		result   int
-	}{
-		{"", http.StatusInternalServerError},
-		{"website2/landing-page.html", http.StatusOK},
-		{"website2/docs.html", http.StatusOK},
-	}
-
-	for i := range tests {
-		rec := httptest.NewRecorder()
-		staticHandler(tests[i].filepath).ServeHTTP(rec, req)
-
-		assert.Equal(
-			tests[i].result, rec.Code,
-			fmt.Sprint("filepath", tests[i].filepath),
-		)
-	}
 }
 
 func Test_httpRedirect(t *testing.T) {
@@ -62,6 +39,30 @@ func Test_httpRedirect(t *testing.T) {
 	)
 }
 
+func Test_staticHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	// More files/scenarios can be added here as desired.
+	tests := []struct {
+		filepath string
+		result   int
+	}{
+		{"", http.StatusInternalServerError},
+		{"website2/landing-page.html", http.StatusOK},
+		{"website2/docs.html", http.StatusOK},
+	}
+
+	for i := range tests {
+		rec := httptest.NewRecorder()
+		staticHandler(tests[i].filepath).ServeHTTP(rec, req)
+
+		assert.Equal(
+			tests[i].result, rec.Code,
+			fmt.Sprint("filepath", tests[i].filepath),
+		)
+	}
+}
+
 func Test_setupCompleteHandler(t *testing.T) {
 	assert := assert.New(t)
 	rec := httptest.NewRecorder()
@@ -78,10 +79,15 @@ func Test_setupCompleteHandler(t *testing.T) {
 		),
 	)
 
+	setup, err := ioutil.ReadFile("website2/setup-complete.html")
+	if err != nil {
+		t.Errorf("Error reading from setup-complete file")
+	}
+
 	// Check that the response body is correct.
 	assert.Equal(
 		rec.Body.String(),
-		setup,
+		string(setup),
 		fmt.Sprintf(
 			"incorrect response body\n%v",
 			rec.Body.String(),
