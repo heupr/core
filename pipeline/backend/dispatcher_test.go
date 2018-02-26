19872a976ebc882123fb9b70417da2119748e6ac
forstmeier <john.forstmeier@gmail.com>
2018-02-25 19:21:38 -0500

    Minor unit test for dispatcher
    Description:
    I'm not entirely sure how best to test this particular method since
    there is no returned value/changes that are realized outside of the
    function scope (since the goroutines for the workers are spun up
    inside the method itself). However, this does appear to be passing
    and I may circle back to this one to figure out a better and more
    comprehensive unit test.
    
    Note: Unit test update
    Note: This check-in includes a known unit test panic

--- pipeline/backend/dispatcher_test.go
+++ pipeline/backend/dispatcher_test.go
@@ -1,1 +1,41 @@
 package backend
+
+import (
+	"testing"
+
+	"core/pipeline/gateway/conflation"
+)
+
+func TestDispatcher(t *testing.T) {
+	repoID := int64(5555)
+	s := &Server{
+		Repos: &ActiveRepos{
+			Actives: map[int64]*ArchRepo{
+				repoID: &ArchRepo{
+					Hive: &ArchHive{
+						Blender: &Blender{
+							Conflator: &conflation.Conflator{
+								Context: &conflation.Context{
+									Issues: []conflation.ExpandedIssue{
+										conflation.ExpandedIssue{},
+									},
+								},
+								Normalizer: conflation.Normalizer{
+									Context: &conflation.Context{
+										Issues: []conflation.ExpandedIssue{
+											conflation.ExpandedIssue{},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	workload <- &RepoData{
+		RepoID: repoID,
+	}
+	s.Dispatcher(1)
+}
