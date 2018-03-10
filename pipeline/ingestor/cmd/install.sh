b603a388380eeb91b98ede21c819e3c63320b02a
taylor <mike.j.taylor0@gmail.com>
2018-03-10 13:39:43 -0500

    Adding Heupr Integration Key to Ingestor Install Script
    Description: Adding Heupr Integration Key to Ingestor Install Script.

--- pipeline/ingestor/cmd/install.sh
+++ pipeline/ingestor/cmd/install.sh
@@ -3,4 +3,4 @@ cp ./cmd /home/heupr/golang/bin/core/pipeline/ingestor/cmd/cmd
 cp ./startprod.sh /home/heupr/golang/bin/core/pipeline/ingestor/cmd/start.sh
 chmod +x /home/heupr/golang/bin/core/pipeline/ingestor/cmd/start.sh
 cp ./configprod.yaml /home/heupr/golang/bin/core/pipeline/ingestor/cmd/config.yaml
-cp ./heupr.2017-10-04.private-key.pem
+cp ./heupr.2017-10-04.private-key.pem /home/heupr/golang/bin/core/pipeline/ingestor/cmd/heupr.2017-10-04.private-key.pem
